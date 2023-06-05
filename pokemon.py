1f16f7c82cdc1dc4bae13bd90da6c227e11d0533
Anthony <64487707+panissidi@users.noreply.github.com>
2023-06-05 13:47:01 -0400

    Updated pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -1,10 +1,35 @@
-class PokemonCards:
-    def __init__(self, name, type, level, current_health, max_health):
+class Rare_Holofoil_Pokemon_Cards:
+    def __init__(self, name, level, type):
       self.name = name
-      self.type = type
       self.level = level
-      self.current_health = current_health
-      self.max_health = max_health
+      self.type = type
+    
+    def __repr__(self):
+      return "{name} is a level {level} {type} Pokemon.".format(name = self.name, level = self.level, type = self.type)
 
-Blastoise = PokemonCards("Blastoise", "Water", 3, 100, 100)
+Blastoise = Rare_Holofoil_Pokemon_Cards("Blastoise", 3, "water")
 print(Blastoise)
+
+class Uncommon_NonHolofoil_Pokemon_Cards:
+    def __init__(self, name, level, type):
+      self.name = name
+      self.level = level
+      self.type = type
+    
+    def __repr__(self):
+      return "{name} is a level {level} {type} Pokemon.".format(name = self.name, level = self.level, type = self.type)
+
+Wartortle = Uncommon_NonHolofoil_Pokemon_Cards("Wartortle", 2, "water")
+print(Wartortle)
+
+class Common_NonHolofoil_Pokemon_Cards:
+    def __init__(self, name, level, type):
+      self.name = name
+      self.level = level
+      self.type = type
+    
+    def __repr__(self):
+      return "{name} is a level {level} {type} Pokemon.".format(name = self.name, level = self.level, type = self.type)
+
+Squirtle = Common_NonHolofoil_Pokemon_Cards("Squirtle", 1, "water")
+print(Squirtle)
